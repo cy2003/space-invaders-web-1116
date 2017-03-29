e482b91b03524f3adf20c44760958bd0b99e19a2
cy2003 <catyyee@hotmail.com>
2017-03-29 19:38:50 -0400

    cleaner version

--- spaceship.js
+++ spaceship.js
@@ -1,5 +1,5 @@
 class Spaceship {
-  constructor(name, crew, phasers, shields, cloaked = false, warpDrive = `disengaged`, docked = true, phasersCharge = `uncharged`){
+  constructor(name, crew, phasers, shields, cloaked = false, warpDrive = 'disengaged', docked = true, phasersCharge = 'uncharged'){
   this.name = name
   this.crew = crew
   this.phasers = phasers
@@ -12,23 +12,6 @@ class Spaceship {
   for (let i = 0; i < this.crew.length; i++){
     this.crew[i].currentShip = this
   }
+
+  }
 }
-
-}
-  //
-  // this.crew.forEach()
-
-
-//   Dog.prototype.bark = function(){
-//   return "Woof!"
-// }
-  // this.phasersCharge = if (this.crew.length > 0){
-  //   (this.crew.forEach(function(element, index, array) {
-  //     if (element.position ==== `Gunner`) {
-  //       "charged"
-  //       }
-  //     else {
-  //       "uncharged"
-  //       }
-  //     }))
-  // }
